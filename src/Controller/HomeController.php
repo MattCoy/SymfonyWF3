5168e55746a23e7315a7c01ec44a5af52803b9f4
MattCoy <contact@le-web-developpeur.com>
2018-05-04 17:40:30 +0200

    Installation et première page

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,29 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Matthieu
+ * Date: 04/05/2018
+ * Time: 13:54
+ */
+//on déclare le namespace
+namespace App\Controller;
+
+//le use pour la classe Response que l'on utilise dans la méthode
+use Symfony\Component\HttpFoundation\Response;
+//pour pouvoir utiliser les annotations
+use Symfony\Component\Routing\Annotation\Route;
+
+//le nom de notre classe
+class HomeController
+{
+    /**
+     * @Route("/bonjour")
+     */
+    //déclaration de notre méthode
+    public function bonjour(){
+        //ici on écrira du code
+
+        //on envoie une réponse : on affiche bonjour avec un peu de html
+        return new Response('<html><body><strong>Bonjour !</strong></body></html>');
+    }
+}
